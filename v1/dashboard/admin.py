357d8e6da861050af4d1ff41f109f6d7ae36ca53
Sudeesh Narayanan <sudeesh@fairfood.org>
2024-05-14 12:30:28 +0530

    feat: admin models

--- v1/dashboard/admin.py
+++ v1/dashboard/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Theme
+from .models import Intervention
+
+@admin.register(Theme)
+class ThemeAdmin(admin.ModelAdmin):
+    """
+    Admin class for managing Theme model in the Django admin interface.
+    """
+    pass
+
+
+@admin.register(Intervention)
+class InterventionAdmin(admin.ModelAdmin):
+    """
+    Admin class for managing Intervention model in the Django admin interface.
+    """
+    pass
