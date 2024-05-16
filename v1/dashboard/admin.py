bebe8698528aebe26f394c3b2664e758e6d9e30e
Sudeesh Narayanan <sudeesh@fairfood.org>
2024-05-16 10:25:05 +0530

    fix: themes api

--- v1/dashboard/admin.py
+++ v1/dashboard/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
+from django.forms import ModelForm
+from django.forms.widgets import TextInput
 
 from .models import Theme
 from .models import Intervention
+
+class ThemeForm(ModelForm):
+    class Meta:
+        model = Theme
+        fields = '__all__'
+        widgets = {
+            'primary_color': TextInput(attrs={'type': 'color'}),
+        }
 
 @admin.register(Theme)
 class ThemeAdmin(admin.ModelAdmin):
     """
     Admin class for managing Theme model in the Django admin interface.
     """
-    pass
+    form = ThemeForm
 
 
 @admin.register(Intervention)
