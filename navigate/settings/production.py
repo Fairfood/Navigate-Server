07fe99f4104a13b318cd76f9e46039cb7bf683a9
Sudeesh Narayanan <sudeesh@fairfood.org>
2025-01-10 15:47:36 +0530

    patch: added production.py config file

---
+++ navigate/settings/production.py
@@ -0,0 +1,28 @@
+from .base import *  # noqa
+from .base import REST_FRAMEWORK, env
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    ROOT_URL,
+]
+
+CORS_ORIGIN_WHITELIST = (ROOT_URL,)
+
+
+REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"]["anon"] = "500/min"
+
+
+# Media file settings for S3
+
+AWS_DEFAULT_REGION = "eu-central-1"
+AWS_ACCESS_KEY_ID = env.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env.get("AWS_STORAGE_BUCKET_NAME")
+AWS_QUERYSTRING_AUTH = False
+AWS_PRELOAD_METADATA = True
+AWS_DEFAULT_ACL = "public-read"
+DEFAULT_FILE_STORAGE = "s3_folder_storage.s3.DefaultStorage"
+DEFAULT_S3_PATH = "navigate"
+MEDIA_ROOT = "/%s/" % DEFAULT_S3_PATH
+MEDIA_URL = "//%s.s3.amazonaws.com/navigate/" % AWS_STORAGE_BUCKET_NAME
